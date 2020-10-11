d32004330e830f11cef16690649151261fad7025
infsolution <clsinfsolution@gmail.com>
2020-10-11 10:23:23 -0300

    implement channel to account

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -11,14 +11,18 @@ class App extends Component{
       // const response = await fetch('http://localhost:3333/QV40X10DLFULOPQN',myInit);
 
       const ws = Ws('ws://localhost:3333')
-      ws.withApiToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjcsImlhdCI6MTYwMjM2NzU5NH0.XSeubzAQd4w21Pt2H9sodrZt0oSlNW7a3IghPaqjxdE').connect()
+      ws.withApiToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjcsImlhdCI6MTYwMjQyMDg0Mn0.ao_rA7LBuF3WBKeqrXj0X8lTtsvDOxhgjyOuTreujWM').connect()
       const chat = ws.subscribe('notifications')
       chat.on('new:order', ()=>{
+        alert('Novo pedido  ')
         document.location.reload() 
-        console.log('entrou no On')
       })
 
-
+      const card = ws.subscribe('account')
+      card.on('new:card', ()=>{
+        //document.location.reload() 
+        alert('Nova Conta')
+      })
       const options ={
             headers : {
                 'Accept': 'application/json',
